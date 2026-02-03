341b62ccd6e749c6649a869f1aaf4582bebc4633
NeoSolutions2022 <121249965+NeoSolutions2022@users.noreply.github.com>
2026-02-03 11:25:56 -0300

    Restore static params for validar page

--- app/validar/[id]/page.tsx
+++ app/validar/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { ValidateClient } from "./validate-client"
 
+export function generateStaticParams() {
+  return []
+}
+
 export default function ValidatePage() {
   return <ValidateClient />
 }
